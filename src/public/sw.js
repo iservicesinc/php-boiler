d7ecb93ad548e6c5503d45a6584b451f7676fd7b
Jesse <60900281+jgrobstein@users.noreply.github.com>
2021-06-10 14:25:59 -0400

    added sw.js and minor fixes

---
+++ src/public/sw.js
@@ -0,0 +1,77 @@
+const CACHE_VERSION = 10;
+const CURRENT_CACHE = `isinc-${CACHE_VERSION}`;
+
+// these are the routes we are going to cache for offline support
+const cacheFiles = [
+                '/',
+                '/images/build.svg',
+                '/images/dark_mode.svg',
+                '/images/is_logo.svg',
+                '/images/rocket.svg',
+                '/images/rocket_bg.svg',
+                '/js/bundle.js',
+                '/css/bundle.css',
+                '/site.webmanifest'
+            ];
+
+// on activation we clean up the previously registered service workers
+self.addEventListener('activate', evt =>
+  evt.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  )
+);
+
+// on install we download the routes we want to cache for offline
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  )
+);
+
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      cache
+        .match(request)
+        .then(matching => matching || cache.match('/offline/'))
+    );
+
+// cache the current page to make it available for offline
+const update = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      fetch(request).then(response => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    fromNetwork(evt.request, 1000).catch(() => fromCache(evt.request))
+  );
+  evt.waitUntil(update(evt.request));
+});
