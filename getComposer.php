b6e7e15ebe62a8793175870e44c161e29d3c004c
Jesse Grobstein <60900281+jgrobstein@users.noreply.github.com>
2021-07-09 23:31:29 -0400

    Update getComposer.php

--- getComposer.php
+++ getComposer.php
@@ -1,11 +1,26 @@
+#!/usr/bin/env php
 <?php
+/*
+ *  ___    ____                  _               
+ * |_ _|  / ___|  ___ _ ____   _(_) ___ ___  ___ 
+ *  | |   \___ \ / _ \ '__\ \ / / |/ __/ _ \/ __|
+ *  | |    ___) |  __/ |   \ V /| | (_|  __/\__ \
+ * |___|  |____/ \___|_|    \_/ |_|\___\___||___/
+ *             ...when IT matters!				
+ *                                                 
+ * https://iservicesinc.com https://iservicesinc.net
+ * Copyright 2021 I Services, Inc. All rights reserved.
+*/
+$checksum = file_get_contents('https://composer.github.io/installer.sig');
 copy('https://getcomposer.org/installer', 'composer-setup.php');
-if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') {
+if (hash_file('sha384', 'composer-setup.php') === $checksum) {
     echo 'Installer verified';
-    exec('php composer-setup.php');
+    $opt = isset($argv[1]) ? $argv[1] : null;
+    if ($opt == null) exec('php composer-setup.php --filename=composer');
+    if ($opt == '--global') exec('php composer-setup.php --filename=composer --install-dir=/usr/bin');
+    exec('rm composer-setup.php');
 } else {
     echo 'Installer corrupt';
 }
-
 echo PHP_EOL;
 ?>
